5e561676dbe58d38900c4c7015c8f17eab8313d0
Joby Elliott <elhober@unm.edu>
2021-09-09 12:11:46 -0600

    hooder field pretty much working

---
+++ modules/event_commencement/media/digraph-foot.js.d/autocomplete-hooder.js
@@ -0,0 +1,6 @@
+// endpoint for returning hooders
+digraph.autocomplete.hooder = {
+    source: digraph.url + '_fastphp/autocomplete-hooder',
+    source_definitive: digraph.url + '_fastphp/autocomplete-hooder?_definitive=true',
+    autoFocus: true
+};
---
+++ modules/event_commencement/src/Chunks/HooderAutocomplete.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Digraph\Modules\event_commencement\Chunks;
+
+use Digraph\Forms\Fields\AbstractAutocomplete;
+
+class HooderAutocomplete extends AbstractAutocomplete
+{
+    const SOURCE = 'hooder';
+}
