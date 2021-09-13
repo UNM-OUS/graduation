8c679e87535c828ce3d6764a6f8272e9ea8dba92
Joby Elliott <elhober@unm.edu>
2021-09-13 10:55:36 -0600

    performance wins

--- modules/deactivated_netid_signin/routes/_oldnetid@all/display.php
+++ modules/deactivated_netid_signin/routes/_oldnetid@all/display.php
@@ -1,16 +1,46 @@
 <?php
+
+use Digraph\Forms\Form;
+use Digraph\Modules\ous_digraph_module\Fields\EmailOrNetID;
+
 $package->cache_noStore();
-$package['fields.page_name'] = 'Class of 2020 sign-in';
+$package['fields.page_name'] = 'Deactivated NetID sign-in';
+/** @var Digraph\Modules\deactivated_netid_signin\DeactivatedNetidHelper */
+$helper = $cms->helper('deactivated-netids');
+
+if ($cms->helper('users')->user()) {
+    $cms->helper('notifications')->printConfirmation('You are already signed in');
+    return;
+}
+
 $url = $cms->helper('urls')->parse('_user/signin');
-$cms->helper('notifications')->printNotice("If your NetID is still active and you remember the password, it is easier to <a href='$url'>sign in with it instead</a>.")
+$cms->helper('notifications')->printNotice("If your NetID is still active and you remember the password, it is much easier to <a href='$url'>sign in with it instead</a>.")
 ?>
 <p>
-    This system is to allow users who have graduated and no longer have access to their UNM NetID to sign into this site.
-    To sign in this way, you need to use the link you received in your non-UNM email.
+    This form is for students who already graduated and no longer have access to their UNM NetID to sign into this site and register for commencement.
+    To sign in, you need either your old NetID username (the first half of your UNM email address, not your Banner ID number), or your non-UNM email address.
+    If we have a matching record on file you will be emailed a sign-in link to a non-UNM email address.
 </p>
+<?php
+
+$form = new Form('');
+$form['netid'] = new EmailOrNetID('NetID or non-UNM email address');
+$form['netid']->required(true);
+$form['netid']->addValidatorFunction(
+    'helper-validation',
+    function (EmailOrNetID $field) use ($helper) {
+        $value = $field->value();
+        return "No matching record for that NetID or email address was found. Please try a different email address, or see below for how to contact us for access.";
+    }
+);
+if ($form->handle()) {
+}
+echo $form;
+
+?>
 <p>
-    We attempted to gather as many non-UNM emails from 2020 graduates as possible, but it was not possible to match all graduates to an outside email address.
-    If you did not receive an email, please contact the Office of the Secretary at <a href='mailto:graduation@unm.edu'>graduation@unm.edu</a> or (505) 277-4664.
+    We attempted to gather as many non-UNM emails from 2020 graduates as we could, but it was not possible to match all graduates to an outside email address.
+    If you can't use this form, please contact the Office of the Secretary at <a href='mailto:graduation@unm.edu'>graduation@unm.edu</a> or (505) 277-4664.
     You will need to provide:
 </p>
 <ul>
