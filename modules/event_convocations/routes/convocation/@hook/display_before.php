eed37effba65bf2d5f701758d0e533f26ea9eb4d
Joby Elliott <elhober@unm.edu>
2021-10-14 13:03:35 -0600

    UI fix for convocations

--- modules/event_convocations/routes/convocation/@hook/display_before.php
+++ modules/event_convocations/routes/convocation/@hook/display_before.php
@@ -5,7 +5,4 @@ $event = $package->noun();
 if ($event['cancelled']) {
     $cms->helper('notifications')->printWarning('This event has been CANCELLED');
 }
-echo "<div class='digraph-card incidental'>";
-echo $event->metaCell();
-echo '<div>Managed by: ' . $event->organization()->link() . '</div>';
-echo "</div>";
+echo '<p class="incidental">Managed by: ' . $event->organization()->link() . '</p>';
