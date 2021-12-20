1a3c732b6f19c0cc58e72f61a8fe99f82337c9e4
Joby Elliott <code@byjoby.com>
2021-12-20 09:07:34 -0700

    removing internal signin system

--- modules/internal_signin/media/digraph-foot.js.d/autocomplete-pastdegreeuser.js
+++
@@ -1,6 +0,0 @@
-// endpoint for returning past degree users
-digraph.autocomplete.pastdegreeuser = {
-    source: digraph.url + '_fastphp/autocomplete-pastdegreeuser',
-    source_definitive: digraph.url + '_fastphp/autocomplete-pastdegreeuser?_definitive=true',
-    autoFocus: true
-};
--- modules/internal_signin/routes/_user@hook/pastdegree/signup_form_post.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Digraph\Modules\internal_signin\Users\PastDegreeUserField;
-
-unset($form['displayname']);
-
-$form['person'] = new PastDegreeUserField('Associated past degree/identity');
-$form['person']->required(true);
